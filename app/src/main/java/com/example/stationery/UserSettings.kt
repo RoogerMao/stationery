b26fdfffc79d8fa0ffe47a57284b2d6339d3be77
RogerM <rogermao2019@gmail.com>
2024-09-05 16:11:38 -0400

    added alignment and formating to user settings page

--- app/src/main/java/com/example/stationery/UserSettings.kt
+++ app/src/main/java/com/example/stationery/UserSettings.kt
@@ -1,34 +1,69 @@
 package com.example.stationery
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+fun UserSettingsScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        UserProfile()
+        UserSettingsSwitches()
+    }
+}
+
+@Composable
 fun UserProfile() {
-    Column() {
-        Image(
-            painter = painterResource(id = R.drawable.default_profile_photo),
-            contentDescription = null
-        )
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.default_profile_photo),
+                contentDescription = null
+            )
+            Button(
+                onClick = {},
+                content = {
+
+                }
+            )
+        }
         Text(
-            text = stringResource(id = R.string.default_username)
+            text = stringResource(id = R.string.default_username),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
 
 @Composable
-fun SwitchSetting() {
-    Row {
+fun SwitchSetting(label: String) {
+    Row (
+        horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Text(
-            text = "Sample Setting #1"
+            text = label
         )
         Switch(
             checked = true,
@@ -40,13 +75,13 @@ fun SwitchSetting() {
 @Composable
 fun UserSettingsSwitches() {
     Column {
-        SwitchSetting()
-        SwitchSetting()
+        SwitchSetting(stringResource(id = R.string.toggle_insights_label))
+        SwitchSetting(stringResource(id = R.string.toggle_summaries_label))
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun UserSettingsPreview() {
-    UserProfile()
+    UserSettingsScreen()
 }
