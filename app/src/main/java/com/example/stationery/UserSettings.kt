483813eafe0cacb08ef4aa6e3d2ffbd1d24166de
RogerM <rogermao2019@gmail.com>
2024-09-11 21:09:50 -0400

    added functions to display dialog + clickable icon that displays a dialogue
    TO-DO:
    - add buttons for making the dialog go away
    - create a view w/ a textfield to get user input for their desired change name

--- app/src/main/java/com/example/stationery/UserSettings.kt
+++ app/src/main/java/com/example/stationery/UserSettings.kt
@@ -1,14 +1,18 @@
 package com.example.stationery
 
-import android.util.Log
+import android.app.AlertDialog
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,19 +20,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun UserSettingsScreen(
     userSettingsViewModel: UserSettingsViewModel = viewModel(),
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
     val userSettingsUIState by userSettingsViewModel.userSettingsUIState.collectAsState()
-
     Column (
         modifier = Modifier.fillMaxSize()
     ) {
@@ -39,6 +45,16 @@ fun UserSettingsScreen(
             displayInsights = userSettingsUIState.displayInsights,
             toggleDisplayInsights = { userSettingsViewModel.toggleInsightDisplay() }
         )
+    }
+
+    if(userSettingsViewModel.showEditUsernameDialog) {
+        val editUsernameBuilder = AlertDialog.Builder(LocalContext.current)
+        editUsernameBuilder
+            .setTitle("Change username")
+            // TO DO MAKE A VIEW TO GET TEXTFIELD, then use setView()
+
+        val editUsernameDialog = editUsernameBuilder.create()
+        editUsernameDialog.show()
     }
 }
 
@@ -61,16 +77,27 @@ fun UserProfile(
                 }
             )
         }
-        Row {
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
             Text(
                 text = stringResource(id = R.string.default_username),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
+                textAlign = TextAlign.Center
             )
-            Button(
-                onClick = {},
-                content = {
-                }
+            Spacer(
+                modifier = Modifier.width(8.dp)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_edit_24),
+                contentDescription = stringResource(id = R.string.edit_username),
+                tint = Color.Black,
+                modifier = Modifier.clickable(
+                    onClick = {
+                        userSettingsViewModel.onShowEditUsernameDialog()
+                    }
+                )
             )
         }
     }
@@ -93,10 +120,6 @@ fun SwitchSetting(
         Switch(
             checked = value,
             onCheckedChange = { toggleFunction() }
-        )
-        Log.d(
-            "SWITCH",
-            label + " = $value"
         )
     }
 }
@@ -125,5 +148,5 @@ fun UserSettingsSwitches(
 @Preview(showBackground = true)
 @Composable
 fun UserSettingsPreview() {
-    //UserSettingsScreen()
+    UserSettingsScreen()
 }
--- app/src/main/java/com/example/stationery/UserSettingsViewModel.kt
+++ app/src/main/java/com/example/stationery/UserSettingsViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.stationery
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.stationery.data.UserSettings
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,6 +13,10 @@ import kotlinx.coroutines.flow.update
 
 class UserSettingsViewModel : ViewModel() {
     private val _userSettingsUIState = MutableStateFlow(UserSettings())
+    var showEditUsernameDialog by mutableStateOf(false)
+        private set
+
+
     val userSettingsUIState: StateFlow<UserSettings> = _userSettingsUIState.asStateFlow()
 
     fun updateUsername(newUsername: Int) {
@@ -33,5 +41,13 @@ class UserSettingsViewModel : ViewModel() {
                 displaySummaries = !currentState.displaySummaries
             )
         }
+    }
+
+    fun onShowEditUsernameDialog() {
+        showEditUsernameDialog = true
+    }
+
+    fun onDismissEditUsernameDialog() {
+        showEditUsernameDialog = false
     }
 }
